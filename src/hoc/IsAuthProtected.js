8d571121a1e54314b5a7e6bb1a75fb3f92279f31
G1304 <georgegagishvili@gmail.com>
2021-07-06 21:29:42 +0400

    HOC fixes

--- src/hoc/IsAuthProtected.js
+++ src/hoc/IsAuthProtected.js
@@ -1,13 +1,8 @@
-import { useEffect } from "react/cjs/react.development";
 import { Redirect } from "react-router";
 
 export const IsAuthProtected = (Component) => {
   const IsAuthProtected = (props) => {
-    useEffect(() => {
-      console.log("You're logged in!");
-    }, []);
-
-    const token = localStorage.getItem("auth.token");
+    const token = JSON.parse(localStorage.getItem("auth.token"));
     if (!token) return <Redirect to="/auth"></Redirect>;
 
     return <Component {...props}></Component>;
--- src/hoc/IsNotAuthProtected.js
+++ src/hoc/IsNotAuthProtected.js
@@ -1,13 +1,8 @@
-import { useEffect } from "react/cjs/react.development";
 import { Redirect } from "react-router";
 
 export const IsNotAuthProtected = (Component) => {
   const IsNotAuthProtected = (props) => {
-    useEffect(() => {
-      console.log("//");
-    }, []);
-
-    const token = localStorage.getItem("auth.token");
+    const token = JSON.parse(localStorage.getItem("auth.token"));
     if (token) return <Redirect to="/"></Redirect>;
 
     return <Component {...props}></Component>;
